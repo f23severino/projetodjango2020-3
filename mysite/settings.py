334ffa2440b803cac462f28b79ab84bcd3e07a65
Jean Charles Severino <f23severino@gmail.com>
2020-10-12 23:34:35 -0300

    Projeto Django 2 - versão 2.9

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+from .local_settings import *
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -18,12 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'poen95q+9(t0pcz^torh-(23de3g=mz9#du9tmfx7g*&y)x)ea'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -75,23 +71,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
 	'default': {
     	'ENGINE': 'django.db.backends.mysql',
-    	'NAME': 'blog',
-    	'USER': 'bloguser',
-    	'PASSWORD': '#Meu-projet0@f1nal#',
-        'HOST': '/var/run/mysqld/mysqld.sock',
-#    	'HOST': 'localhost',
-    	'PORT': '3306',
-#       'PORT': '',
+    	'NAME': DB_NAME,
+    	'USER': DB_USER,
+    	'PASSWORD': DB_PASSWORD,
+    	'HOST': DB_HOST,
+    	'PORT': DB_PORT,
 	}
 }
 
